d3542d12061f19397dd7b325fd0617a756ee2db0
lucasbrumatti99@gmail <lucasbruma08@gmail.com>
2022-11-23 23:42:35 -0300

    agrego feature multi-idioma

---
+++ script.js
@@ -0,0 +1,23 @@
+const flagsElement = document.getElementById("flags");
+const textsToChange = document.querySelectorAll('[data-section]');
+
+const changeLanguage = async language =>{
+    const requestJson = await fetch(`./idiomas/${language}.json`);
+    const texts = await requestJson.json();
+
+    for(const textToChange of textsToChange){
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        const secondValue = textToChange.dataset.secondvalue;
+        
+        console.log(textToChange);
+        console.log(secondValue);
+
+        if(secondValue == null) textToChange.innerHTML = texts[section][value];
+        else textToChange.innerHTML = texts[section][value][secondValue];
+    }
+}
+
+flagsElement.addEventListener("click", (e) =>{
+    changeLanguage(e.target.parentElement.dataset.language)
+})
